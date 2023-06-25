0ddff20d3b5a7e06bf8db3d97e53fb299ffb33bb
Trần Quang <quangtranhhpt1@gmail.com>
2023-06-25 12:21:00 +0700

    Add project files.

---
+++ Demo_Web_Security/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Demo_Web_Security.DTO;
+using Demo_Web_Security.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Demo_Web_Security.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private static int currentMoney = 10000000;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.price = currentMoney;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Transfer(TransferDTO dto)
+        {
+            currentMoney = currentMoney - dto.price;
+            string mess = "Chuyển thành công số tiền " + dto.price + " từ tài khoản " + dto.accountFrom + " tới tài khoản " + dto.accountTo;
+            ViewBag.mess = mess;
+            ViewBag.price = currentMoney;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Demo_Web_Security/DTO/TransferDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Demo_Web_Security.DTO
+{
+    public class TransferDTO
+    {
+        public int accountFrom { get; set; }
+        public int accountTo { get; set; }
+        public int price { get; set; }
+    }
+}
